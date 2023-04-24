9ae37fa51732389568e13eaef63aeeb4f2b33401
Harsh <hackwidharsh@gmail.com>
2023-04-24 19:36:39 +0530

    added keyword search for dadjoke

--- cmd/README.md
+++ cmd/README.md
@@ -1,1 +1,7 @@
 This is a CLI application which returns a random dadjoke in the terminal window. This is built using cobra-cli & golang using the api: https://icanhazdadjoke.com/api#calling-the-api
+
+How to run:
+1. go build main.go
+2. go install 
+3. dadjoke random (fetches a random dadjoke)
+4. dadjoke random --keyword="xyz" (fetches a dadjoke containing the specific keyword.)
--- cmd/random.go
+++ cmd/random.go
@@ -8,7 +8,9 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"math/rand"
 	"net/http"
+	"time"
 
 	"github.com/spf13/cobra"
 )
@@ -19,18 +21,32 @@ var randomCmd = &cobra.Command{
 	Short: "Returns a random dadjoke.",
 	Long:  `Returns a random dadjoke.`,
 	Run: func(cmd *cobra.Command, args []string) {
+		JokeKeyword, _ := cmd.Flags().GetString("keyword")
+		if JokeKeyword != "" {
+			getRandomJokeWithKeyword(JokeKeyword)
+		} else {
+			getRandomJoke()
+		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(randomCmd)
-	getRandomJoke()
+
+	randomCmd.PersistentFlags().String("keyword", "", "Dadjoke for a specific word.")
 }
 
 type Joke struct {
 	ID     string `json:"id"`
 	Joke   string `json:"joke"`
 	Status int    `json:"status"`
+}
+
+type SearchResponse struct {
+	Results       json.RawMessage `json:"results"`
+	SearchKeyword string          `json:"search_term"`
+	Status        int             `json:"status"`
+	TotalJokes    int             `json:"total_jokes"`
 }
 
 func getRandomJoke() {
@@ -62,4 +78,45 @@ func getJokeData(baseAPI string) []byte {
 		log.Printf("Unable to read the response body %v", err)
 	}
 	return responseBytes
+}
+func getJokeDataWithKeyword(jokeKeyWord string) (totalJokes int, jokeList []Joke) {
+
+	url := fmt.Sprintf("https://icanhazdadjoke.com/search?term=%s", jokeKeyWord)
+	responseBytes := getJokeData(url)
+	jokeListRaw := SearchResponse{}
+
+	err := json.Unmarshal(responseBytes, &jokeListRaw)
+	if err != nil {
+		log.Printf("Unable to do unmarshall of json data - %v", err)
+	}
+
+	jokes := []Joke{}
+
+	err1 := json.Unmarshal(jokeListRaw.Results, &jokes)
+	if err != nil {
+		log.Printf("Unable to unmarshal the responses. -%v", err1)
+	}
+	return jokeListRaw.TotalJokes, jokes
+
+}
+
+func getRandomJokeWithKeyword(jokeKeyword string) {
+	total, results := getJokeDataWithKeyword(jokeKeyword)
+	randomiser(total, results)
+	//fmt.Println(results)
+}
+
+func randomiser(length int, jokeList []Joke) {
+	rand.Seed(time.Now().Unix())
+	min := 0
+	max := length - 1
+
+	if length <= 0 {
+		err := fmt.Errorf("unable to find a dadjoke with this keyword")
+		fmt.Println(err)
+	} else {
+		randomNumber := min + rand.Intn(max-min)
+		fmt.Println(jokeList[randomNumber].Joke)
+	}
+
 }
